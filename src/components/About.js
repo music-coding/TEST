8b5a66f21ce8bd442df564503d948ed29a92e52b
Code <code@Josiahs-MacBook-Pro.local>
2017-07-22 19:01:07 -0500

    First commit

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const About = (props) => (
+  <div className="main-content">
+    <h2>{ props.title }</h2>
+    <p>This is going to be a website for someone and it will be part of my portolio</p>
+  </div>
+);
+
+export default About;
---
+++ src/data/teachers.js
@@ -0,0 +1,22 @@
+const TeacherList = [
+  {
+    name: "Becky Zigo",
+    bio: "Becky is the nicest person in the world!!",
+    img_src: "https://s-media-cache-ak0.pinimg.com/avatars/beckyberndt-1366604011_280.jpg",
+    id: "teach-1"
+  },
+  {
+    name: "Josiah Zigo",
+    bio: "'Josiah is the King to the queen Becky",
+    img_src: "https://lh3.googleusercontent.com/-MKF0NfdzYmQ/AAAAAAAAAAI/AAAAAAAAAz8/drxZy60IxsM/s640/photo.jpg",
+    id: "teach-2"
+  },
+  {
+    name: "Geo 'Lo' Cation",
+    bio: "Geo is a JavaScript developer working on large-scale applications. He's also a teacher who strives to support students in removing all barriers to learning code.",
+    img_src: "http://treehouse-code-samples.s3.amazonaws.com/bootstrap-4/img/geo.png",
+    id: "teach-3"
+  }
+];
+
+export default TeacherList;
